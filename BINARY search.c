e3809320be64533251442467bbd6ba353084df80
Arun <99261603+Arunitdy@users.noreply.github.com>
2024-12-16 17:21:37 +0530

    Add files via upload

---
+++ BINARY search.c
@@ -0,0 +1,39 @@
+
+#include<stdio.h>
+int main()
+{   int n;
+
+    printf("enter the size of array:");
+    scanf("%d",&n);
+      int a[n];
+    printf("enter the elements:\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",& a[i]);
+    }
+    int t;
+    printf("enter the data need to find:");
+    scanf("%d",&t);
+    int s=0;
+    int e=n-1;
+    while(s<=e)
+    {
+        int mid=s+(e-s) /2;
+        if(a[mid]==t)
+        {
+            printf("element at%d",mid);
+            break;
+        }
+        else if(a[mid]<t)
+        {
+            s=mid+1;
+        }
+        else
+        {
+            e=mid-1;
+        }
+    }
+    printf("element not found");
+}
+
+
